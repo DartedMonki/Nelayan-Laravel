92e0cd43b37efce654501c1a96b1b1cf2b6e5a7d
Afriyadi Yanuar R <afriyadi_dani@apps.ipb.ac.id>
2020-01-10 00:55:22 +0700

    Init with fully working auth

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.app', ['page' => __('Profil User'), 'pageSlug' => 'profile'])
+
+@section('content')
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-user">
+                <div class="card-body">
+                    <p class="card-text">
+                        <div class="author">
+                            <div class="block block-one"></div>
+                            <div class="block block-two"></div>
+                            <div class="block block-three"></div>
+                            <div class="block block-four"></div>
+                            <a href="#">
+                                <img class="avatar" src="{{ asset('black') }}/img/emilyz.jpg" alt="">
+                                <h5 class="title">{{ auth()->user()->name }}</h5>
+                            </a>
+                            <p class="description">
+                                @if (auth()->user()->status == 'admin')
+                                {{ __('Admin') }}
+                                @elseif (auth()->user()->status == 'ketua')
+                                {{ __('Ketua Kelompok') }}
+                                @else 
+                                {{ __('Pembudidaya') }}
+                                @endif
+                            </p>
+                        </div>
+                    </p>
+                    <div class="card-description">
+                        {{ __('Do not be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...') }}
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <div class="button-container">
+                        <button class="btn btn-icon btn-round btn-facebook">
+                            <i class="fab fa-facebook"></i>
+                        </button>
+                        <button class="btn btn-icon btn-round btn-twitter">
+                            <i class="fab fa-twitter"></i>
+                        </button>
+                        <button class="btn btn-icon btn-round btn-google">
+                            <i class="fab fa-google-plus"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="title">{{ __('Edit Profil') }}</h5>
+                </div>
+                <form method="post" action="{{ route('profile.update') }}" autocomplete="off">
+                    <div class="card-body">
+                            @csrf
+                            @method('put')
+
+                            @include('alerts.success')
+
+                            <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                                <label>{{ __('Nama') }}</label>
+                                <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Nama') }}" value="{{ old('name', auth()->user()->name) }}">
+                                @include('alerts.feedback', ['field' => 'name'])
+                            </div>
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                <label>{{ __('Alamat Email') }}</label>
+                                <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Alamat Email') }}" value="{{ old('email', auth()->user()->email) }}">
+                                @include('alerts.feedback', ['field' => 'email'])
+                            </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-fill btn-primary">{{ __('Simpan') }}</button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="title">{{ __('Ubah Password') }}</h5>
+                </div>
+                <form method="post" action="{{ route('profile.password') }}" autocomplete="off">
+                    <div class="card-body">
+                        @csrf
+                        @method('put')
+
+                        @include('alerts.success', ['key' => 'password_status'])
+
+                        <div class="form-group{{ $errors->has('old_password') ? ' has-danger' : '' }}">
+                            <label>{{ __('Password Saat Ini') }}</label>
+                            <input type="password" name="old_password" class="form-control{{ $errors->has('old_password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password Saat Ini') }}" value="" required>
+                            @include('alerts.feedback', ['field' => 'old_password'])
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            <label>{{ __('Password Baru') }}</label>
+                            <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password Baru') }}" value="" required>
+                            @include('alerts.feedback', ['field' => 'password'])
+                        </div>
+                        <div class="form-group">
+                            <label>{{ __('Konfirmasi Password Baru') }}</label>
+                            <input type="password" name="password_confirmation" class="form-control" placeholder="{{ __('Konfirmasi Password Baru') }}" value="" required>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-fill btn-primary">{{ __('Ubah password') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="header py-7 py-lg-8">
+        <div class="container">
+            <div class="header-body text-center mb-7">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 col-md-6">
+                        <h1 class="text-white">{{ __('Nelayan Cerdas K1000') }}</h1>
+                        <p class="text-lead text-light">
+                            {{ __('Aplikasi untuk membantu nelayan.') }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
