c3cee137dac1a00cdb4c28088983e81e52be2943
MarsaMD <mmdaffa@gmail.com>
2020-02-06 22:22:53 +0700

    pencucian_pemindahan

--- database/migrations/2020_02_06_215851_create_pemindahan_table.php
+++ database/migrations/2020_02_06_215851_create_pemindahan_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProduksiTable extends Migration
+class CreatePemindahanTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,21 +13,17 @@ class CreateProduksiTable extends Migration
      */
     public function up()
     {
-        Schema::create('produksi', function (Blueprint $table) {
+        Schema::create('pemindahan', function (Blueprint $table) {
             $table->Increments('id');
-            $table->string('nama_ikan')->nullable();
             $table->integer('jumlah_ikan')->nullable();
             $table->integer('panjang_ikan')->nullable();
-            $table->date('tanggal_cuci')->nullable();
-            $table->date('tanggal_tebar')->nullable();
             $table->date('tanggal_pindah')->nullable();
-            $table->date('tanggal_panen')->nullable();
-            $table->string('status_panen')->default('Pembesaran');
             $table->integer('keramba_id')->unsigned();
             $table->foreign('keramba_id')->references('id')->on('keramba');
+            $table->integer('produksi_id')->unsigned();
+            $table->foreign('produksi_id')->references('id')->on('produksi');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->timestamps();
         });
     }
 
@@ -38,6 +34,6 @@ class CreateProduksiTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produksi');
+        Schema::dropIfExists('pemindahan');
     }
 }
